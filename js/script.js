cd679d6275d8e78ea538baa8ebe2d6308e1055ed
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-01-11 12:15:28 +0100

    script.js | Edit | Trying paralax with scroll magic

--- js/script.js
+++ js/script.js
@@ -1,15 +1,48 @@
-var controller = new ScrollMagic.Controller();
+var controller = new ScrollMagic.Controller()
 
-var sceneHome = new ScrollMagic.Scene({
-  offset: 70,
-  duration: 500
-})
-.setTween('#cad-page-1', {opacity: 0})
-.addTo(controller);
+function homeOpacityAni() {
+  var sceneHome = new ScrollMagic.Scene({
+    offset: 10,
+    duration: 500
+  })
+    .setTween('#cad-page-1', { opacity: 0 })
+    .addTo(controller)
+}
 
-var scene2 = new ScrollMagic.Scene({
-  offset: 100,
-  duration: 500
-})
-.setTween('#cad-page-2', {opacity: 1})
-.addTo(controller);
+function homeTopAni() {
+  var timelineHome = new TimelineMax()
+
+  var tween1 = TweenMax.to('#cad-page-1 > header', 2, {
+    position: 'relative',
+    bottom: 100
+  })
+
+  var tween2 = TweenMax.to('#cad-page-1 > p', 2, {
+    position: 'relative',
+    bottom: 100
+  })
+
+  var sceneHome2 = new ScrollMagic.Scene({
+    offset: 30,
+    duration: 300
+  })
+
+  timelineHome.add(tween1).add(tween2)
+  sceneHome2
+    .setTween(timelineHome)
+    .addIndicators({ name: 'loop' })
+    .addTo(controller)
+}
+
+function page2OpacityAni() {
+  var scene2 = new ScrollMagic.Scene({
+    offset: 100,
+    duration: 500
+  })
+    .setTween('#cad-page-2', { opacity: 1 })
+    .addTo(controller)
+}
+
+homeOpacityAni()
+homeTopAni()
+page2OpacityAni()
