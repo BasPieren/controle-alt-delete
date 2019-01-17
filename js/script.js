a4f8d0496319088124c02a982d63428d033004d1
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-01-17 14:57:57 +0100

    script.js | Edit | Added simple JQUERY script

--- js/script.js
+++ js/script.js
@@ -1,39 +1,16 @@
 var controller = new ScrollMagic.Controller()
 
-// INTRO
-var sceneIntro = new ScrollMagic.Scene({
-  offset: 50,
-  duration: 200
-})
-  .setTween('#cad-intro-page *', { opacity: 0})
-  .addTo(controller)
+$('.cad-fade-in').each(function() {
 
-// PAGE 1
-var scenePage1 = new ScrollMagic.Scene({
-  offset: 225,
-  duration: 200
-})
-  .setTween('#cad-page-1 > *', { opacity: 1 })
-  .addTo(controller)
+  var tween = TweenMax.from($(this), 0.6, {
+    opacity: 0,
+    ease: Linear.easeNone
+  });
 
-var scenePage1Out = new ScrollMagic.Scene({
-  offset: 650,
-  duration: 200
-})
-  .setTween('#cad-page-1 > *', { opacity: 0 })
-  .addTo(controller)
+  var scene = new ScrollMagic.Scene({
+    triggerElement: this
+  })
+  .setTween(tween)
+  .addTo(controller);
 
-// PAGE 2
-var scenePage1 = new ScrollMagic.Scene({
-  offset: 825,
-  duration: 200
-})
-  .setTween('#cad-page-2 > *', { opacity: 1 })
-  .addTo(controller)
-
-var scenePage1Out = new ScrollMagic.Scene({
-  offset: 1150,
-  duration: 200
-})
-  .setTween('#cad-page-2 > *', { opacity: 0 })
-  .addTo(controller)
+});
