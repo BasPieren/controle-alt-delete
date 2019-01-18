b58422b61aded3b36595a4da43fb92b9f816d1d3
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-01-18 11:35:24 +0100

    script.js | Edit | Added population per unit animation

--- js/script.js
+++ js/script.js
@@ -4,29 +4,49 @@ function sectionFadeIn() {
   const classFadeIn = document.getElementsByClassName('cad-fade-in')
 
   for (var i = 0; i < classFadeIn.length; i++) {
-    var tweenFadeIn = TweenMax.from(classFadeIn[i], 0.5, {
+    var tween = TweenMax.from(classFadeIn[i], 0.5, {
       opacity: 0,
       ease: Linear.easeNone
     })
 
-    var sceneFadeIn = new ScrollMagic.Scene({
+    var scene = new ScrollMagic.Scene({
       triggerElement: classFadeIn[i]
     })
-      .setTween(tweenFadeIn)
+      .setTween(tween)
       .addTo(controller)
   }
 }
 
 sectionFadeIn()
 
-var tween = TweenMax.from('.cad-population-icon', 1, {
-  scale: 0,
-  ease: Linear.easeNone
-})
+function populationTotal() {
+  var tween = TweenMax.from('.cad-population-icon', 1, {
+    scale: 0,
+    ease: Linear.easeNone
+  })
 
-var scene = new ScrollMagic.Scene({
-  triggerElement: "#cad-page-2",
-  duration: 300
-})
-	.setTween(tween)
-	.addTo(controller);
+  var scene = new ScrollMagic.Scene({
+    triggerElement: '#cad-page-2',
+    duration: 300
+  })
+    .setTween(tween)
+    .addTo(controller)
+}
+
+populationTotal()
+
+function populationUnit() {
+  var tween = TweenMax.from('.cad-horizontal-population', 1, {
+    width: 0,
+    ease: Linear.easeNone
+  })
+
+  var scene = new ScrollMagic.Scene({
+    triggerElement: '#cad-page-7',
+    duration: 300
+  })
+    .setTween(tween)
+    .addTo(controller)
+}
+
+populationUnit()
